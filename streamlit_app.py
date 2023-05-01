0118897993b95be6473ce9a9b6be7e790a60c4c5
Pawel Stasinski <poczta130@gmail.com>
2023-05-01 20:40:23 +0100

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+from streamlit_gallery import apps, components
+from streamlit_gallery.utils.page import page_group
+
+def main():
+    page = page_group("p")
+
+
+    if 'streamlit_gallery.utils.page_page_group_p' not in st.session_state:
+        st.session_state['streamlit_gallery.utils.page_page_group_p'] = 0
+
+    with st.sidebar:
+        st.title("🎈 Okld's Gallery")
+
+        with st.expander("✨ APPS", True):
+            page.item("Streamlit gallery", apps.gallery, default=True)
+
+        with st.expander("🧩 COMPONENTS", True):
+            page.item("Ace editor", components.ace_editor)
+            page.item("Disqus", components.disqus)
+            page.item("Elements⭐", components.elements)
+            page.item("Pandas profiling", components.pandas_profiling)
+            page.item("Quill editor", components.quill_editor)
+            page.item("React player", components.react_player)
+
+    page.show()
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="Streamlit Gallery by Okld", page_icon="🎈", layout="wide")
+    main()
