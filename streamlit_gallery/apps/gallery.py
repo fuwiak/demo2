2c68c2a297b383391f9d5c6755887ebafe616a03
Pawel Stasinski <poczta130@gmail.com>
2023-05-01 21:08:27 +0100

    add README

--- streamlit_gallery/apps/gallery.py
+++ streamlit_gallery/apps/gallery.py
@@ -7,6 +7,7 @@ image2 = Image.open('./media/images/AI_logo.png')
 
 
 def main():
+    st.markdown((Path(__file__).parents[2] / "README.md").read_text())
     st.image(image1,
              caption='Creator: User Neil Iris (@neil_ingham) from Unsplash')
     st.image(image2, caption='Powered by Quaantum AI', width=50)
