95425ec19b08f04dc463afdcedfaed68545439b7
Pawel Stasinski <poczta130@gmail.com>
2023-05-01 21:06:18 +0100

    image1,image2

--- streamlit_gallery/apps/gallery.py
+++ streamlit_gallery/apps/gallery.py
@@ -1,10 +1,15 @@
 import streamlit as st
 
-from pathlib import Path
+from PIL import Image
+
+image1 = Image.open('./media/images/mountains.png')
+image2 = Image.open('./media/images/AI_logo.png')
 
 
 def main():
-    st.markdown((Path(__file__).parents[2]/"README.md").read_text())
+    st.image(image1,
+             caption='Creator: User Neil Iris (@neil_ingham) from Unsplash')
+    st.image(image2, caption='Powered by Quaantum AI', width=50)
 
 
 if __name__ == "__main__":
