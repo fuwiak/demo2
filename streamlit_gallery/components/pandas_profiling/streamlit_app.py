fb9be93a4c5e05d328ba5afa2450764ad7f4653a
Pawel Stasinski <poczta130@gmail.com>
2023-05-01 23:50:24 +0100

    upd: streamlit_app.py

--- streamlit_gallery/components/pandas_profiling/streamlit_app.py
+++ streamlit_gallery/components/pandas_profiling/streamlit_app.py
@@ -4,20 +4,20 @@ import streamlit as st
 
 from streamlit_gallery.utils.readme import readme
 from streamlit_pandas_profiling import st_profile_report
-
+import pandasai
 
 def main():
-    with readme("streamlit-pandas-profiling", st_profile_report, __file__):
-        dataset = "https://storage.googleapis.com/tf-datasets/titanic/train.csv"
+    st.write("# Pandas AI")
+    dataset = "https://storage.googleapis.com/tf-datasets/titanic/train.csv"
 
-        df = pd.read_csv(dataset)
-        pr = gen_profile_report(df, explorative=True)
+    df = pd.read_csv(dataset)
+    pr = gen_profile_report(df, explorative=True)
 
-        st.write(f"🔗 [Titanic dataset]({dataset})")
-        st.write(df)
+    st.write(f"🔗 [Titanic dataset]({dataset})")
+    st.write(df)
 
-        with st.expander("REPORT", expanded=True):
-            st_profile_report(pr)
+    with st.expander("REPORT", expanded=True):
+        st_profile_report(pr)
 
 
 @st.cache(allow_output_mutation=True)
